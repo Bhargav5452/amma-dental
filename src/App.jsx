ef091f79298bece67366c213a30e4f05a5bfa70c
Bhargav5452 <bhargavanandan5@gmail.com>
2026-06-08 19:06:39 +0530

    Fix scroll position on page transition using ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,9 +7,20 @@ import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="app-container flex flex-col" style={{ minHeight: '100vh' }}>
+      <ScrollToTop />
       <Header />
       <main style={{ flex: 1 }}>
         <Routes>
